78a382427eb1a96374bf67c0c21e76a534b40188
huka173 <4g5gr55y55y@gmail.com>
2025-01-09 06:40:25 +0300

    Add new dependency and create base javalin project

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,17 @@
 package com.example;
 
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/users", ctx -> {
+            ctx.result("Hello, this is a users page");
+        });
+
+        app.start(7070);
     }
 }
